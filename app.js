e2fb632fc96d338503c443617a1386a7361ddd96
Sonisachin0263 <86377303+Sonisachin0263@users.noreply.github.com>
2025-01-28 11:49:54 +0530

    inial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const moment = require('moment-timezone');
+const app = express();
+
+app.get('/', (req, res) => {
+    const currentTime = moment().tz('Asia/Kolkata').format('YYYY-MM-DD HH:mm:ss');
+    let visitorIp = req.ip;
+    // if (visitorIp.startsWith('::ffff:')) {
+    //     visitorIp = visitorIp.replace('::ffff:', '');
+    // }
+    res.json({
+        timestamp: currentTime,
+        ip: visitorIp
+        
+    });
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
