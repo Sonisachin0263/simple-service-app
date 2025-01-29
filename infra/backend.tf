04466e5787b919b9fbad01dfa7a6d6c9612b2ca8
Sonisachin0263 <86377303+Sonisachin0263@users.noreply.github.com>
2025-01-29 22:22:44 +0530

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "particle-terraform-state"
-#     key    = "terraform/state"
-#     region = "ap-south-1"
-#     encrypt = true
-#     use_lockfile = true
-#   }
-# }
+ terraform {
+   backend "s3" {
+     bucket = "particle-terraform-state"
+     key    = "terraform/state.tfstate"
+     region = "ap-south-1"
+     encrypt = true
+     dynamodb_table = "terraform-lock"
+   }
+}
