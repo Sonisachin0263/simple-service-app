0f4a210fe38bdc4a2be6a58471b1bc6897a18f5c
Sonisachin0263 <86377303+Sonisachin0263@users.noreply.github.com>
2025-01-31 00:53:49 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -17,5 +17,5 @@ app.get('/', (req, res) => {
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Hey! Simple Time Service is running on ${PORT}`);
+    console.log(` Hey! Simple Time Service is running on ${PORT}`);
 });
